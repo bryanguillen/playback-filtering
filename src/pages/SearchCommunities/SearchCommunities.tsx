809d52092434a58a44960b2ec712723ff13f542a
bryanguillen <bryan.guillen217@gmail.com>
2025-03-19 06:55:34 -0400

    Refactor implementation for handling search

--- src/pages/SearchCommunities/SearchCommunities.tsx
+++ src/pages/SearchCommunities/SearchCommunities.tsx
@@ -1,16 +1,12 @@
-import { useState, useEffect } from 'react';
-import {
-  Box,
-  Grid,
-  GridItem,
-  Input,
-  Field,
-  Spinner,
-  Flex,
-} from '@chakra-ui/react';
+import { useState, useCallback } from 'react';
+import { Box, Grid, GridItem, Spinner, Flex } from '@chakra-ui/react';
 
 import { Community } from './types';
-import { SearchCommunitiesFilter, CommunityCard } from './components';
+import {
+  SearchCommunitiesFilter,
+  CommunityCard,
+  SearchCommunitiesInput,
+} from './components';
 
 import { PageHeader } from '../../components';
 
@@ -18,42 +14,23 @@ import mockCommunityResults from './data/mockCommunityResults.json';
 
 export function SearchCommunities() {
   const [isLoading, setIsLoading] = useState(false);
-  const [searchQuery, setSearchQuery] = useState('');
-  
+
   const communityResults: Community[] = mockCommunityResults.channels;
 
-  /**
-   * MOCK: Debounce the search query to simulate a real API call
-   */
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(true);
-      // Simulate an API call or data fetching here
-      setTimeout(() => {
-        setIsLoading(false);
-      }, 1000); // Simulate a 1-second delay
-    }, 500); // Debounce delay of 500ms
-
-    return () => clearTimeout(timer);
-  }, [searchQuery]);
+  // Mock API call to fetch communities
+  const handleSearch = useCallback(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000); // Simulate a 1-second delay
+  }, []);
 
   return (
     <Box p={4}>
       <PageHeader.Root>
         <PageHeader.Title>Search Communities</PageHeader.Title>
         <PageHeader.Actions>
-          <Field.Root>
-            <Field.Label srOnly>Search</Field.Label>
-            <Input
-              placeholder="Search for a community"
-              w={{
-                base: '100%',
-                md: '250px',
-              }}
-              value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)}
-            />
-          </Field.Root>
+          <SearchCommunitiesInput onSearch={handleSearch} />
           <SearchCommunitiesFilter />
         </PageHeader.Actions>
       </PageHeader.Root>
